4d5b3152c7bc19ff1db0b7e659b6a6798f7bde91
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2021-06-29 13:52:24 +0200

    Add project files.

---
+++ Yield/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Yield
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var item in GetNumbers())
+            {
+                Console.WriteLine(item);
+            }
+
+            var numbers = GetNumbers();
+            IEnumerator<int> enumerator = numbers.GetEnumerator();
+
+            while (enumerator.MoveNext())
+            {
+                var item = enumerator.Current;
+                Console.WriteLine(item);
+            }
+
+            foreach (var item in GetNumbersOrdinary().Take(2))
+            {
+                Console.WriteLine(item);
+            } 
+            
+            foreach (var item in GetNumbersYield().Take(2))
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+
+        private static IEnumerable<int> GetNumbersYield()
+        {
+            var i = 0;
+            while (true)
+            {
+                yield return ++i;
+            }
+        }
+
+        private static IEnumerable<int> GetNumbersOrdinary()
+        {
+            int i = 0;
+            var result = new List<int>();
+
+            while (i < 1000)
+            {
+                result.Add(++i);
+            }
+
+            return result;
+        }
+
+        private static IEnumerable<int> GetNumbers()
+        {
+            yield return 1;
+            yield return 2;
+            yield return 3;
+        }
+    }
+}
